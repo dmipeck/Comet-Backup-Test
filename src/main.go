fd11ba28e00fd41876611309827284b91bce0003
David Peck <david@littlemonkey.co.nz>
2022-07-21 05:19:56 +0000

    Show most efficient control method

--- src/main.go
+++ src/main.go
@@ -21,6 +21,45 @@ func (intersection *Intersection) cpsTotal() float32 {
 	return total
 }
 
+type ControlMethod struct {
+	name           string
+	efficiencyHigh float32
+	efficiencyMed  float32
+	efficiencyLow  float32
+}
+
+func (control *ControlMethod) cpsEfficency(intersection Intersection) float32 {
+	cpsTotal := intersection.cpsTotal()
+	if cpsTotal < 10 {
+		return control.efficiencyLow
+	} else if cpsTotal < 20 {
+		return control.efficiencyMed
+	} else {
+		return control.efficiencyHigh
+	}
+}
+
+var controlMethods []ControlMethod = []ControlMethod{
+	{
+		name:           "Roundabout",
+		efficiencyHigh: 0.50,
+		efficiencyMed:  0.75,
+		efficiencyLow:  0.09,
+	},
+	{
+		name:           "Stop Signs",
+		efficiencyHigh: 0.20,
+		efficiencyMed:  0.30,
+		efficiencyLow:  0.40,
+	},
+	{
+		name:           "Traffic Lights",
+		efficiencyHigh: 0.90,
+		efficiencyMed:  0.75,
+		efficiencyLow:  0.30,
+	},
+}
+
 func main() {
 	intersection := Intersection{
 		north: 5,
@@ -28,5 +67,19 @@ func main() {
 		south: 5,
 		west:  5,
 	}
-	fmt.Println(intersection.cpsTotal())
+
+	var bestMethod *ControlMethod
+	var bestEfficency float32
+	var nextMethodEfficency float32
+	for _, nextMethod := range controlMethods {
+		nextMethodEfficency = nextMethod.cpsEfficency(intersection)
+		if bestMethod == nil || nextMethodEfficency < bestEfficency {
+			bestEfficency = nextMethodEfficency
+			bestMethod = &nextMethod
+		}
+	}
+
+	fmt.Printf("Intersection CPM Total: %.2f\n", intersection.cpsTotal())
+	fmt.Printf("Intersection Best Method: %s\n", bestMethod.name)
+	fmt.Printf("Intersection Best Method Efficency: %.2f%%\n", bestMethod.cpsEfficency(intersection))
 }
