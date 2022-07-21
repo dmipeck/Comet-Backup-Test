d765481116db3a0e86a05ad89a6e86891ad4a5c6
David Peck <david@littlemonkey.co.nz>
2022-07-21 04:58:45 +0000

    Calculate cps total for intersection

--- src/main.go
+++ src/main.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
+type Intersection struct {
+	north float32
+	east  float32
+	south float32
+	west  float32
+}
+
+func (intersection *Intersection) allRoads() []float32 {
+	return []float32{intersection.north, intersection.east, intersection.south, intersection.west}
+}
+
+func (intersection *Intersection) cpsTotal() float32 {
+	var total float32 = 0
+	for _, roadCpm := range intersection.allRoads() {
+		total += roadCpm
+	}
+	return total
+}
+
 func main() {
-	fmt.Println("hello world")
+	intersection := Intersection{
+		north: 5,
+		east:  5,
+		south: 5,
+		west:  5,
+	}
+	fmt.Println(intersection.cpsTotal())
 }
